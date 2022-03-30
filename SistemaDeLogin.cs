65b5112d577662bf784d2c71e21c1fe6f34d14b6
renzoalves <djenrolandy@gmail.com>
2022-03-29 23:42:20 -0300

    criado sistema de login

---
+++ SistemaDeLogin.cs
@@ -0,0 +1,14 @@
+digite seu e-mail
+digite sua senha
+
+
+Criar uma nova branch com base na branch principal(master) e já se posicionar nela: <br>
+Nome da nova branch: 'sistema-de-login' <br>
+Nome da branch base: 'master'
+<pre>$ git checkout -b sistema-de-login master</pre>
+
+Copy the token.
+Switch back to VS code.
+Click Signing in to github.com... in the status bar.
+Paste the token and hit enter.
+vscode://vscode.github-authentication/did-authenticate?windowid=1&code=bda1903045ac04ce58fb&state=ebce1c5a-5f02-41da-b195-dbe5ebf2840f
